a4590d12ce83d74b5fdec02e09077d17b77b866b
Jianshen Liu <jliu120@ucsc.edu>
2016-11-10 23:20:25 -0800

    Remove unsupported sentiment check

--- README.md
+++ README.md
@@ -22,20 +22,14 @@ or
 ```python
 from corenlp_pywrap import pywrap
 cn = pywrap.CoreNLP(url='http://localhost:9000', annotator_list=full_annotator_list)
-#full_annotator_list = ["tokenize", "cleanxml", "ssplit", "pos", "lemma", "ner", "regexner", "truecase", "parse", "depparse", "dcoref", "relation", "natlog", "quote"]
+#full_annotator_list = ["tokenize", "cleanxml", "ssplit", "pos", "lemma", "ner", "regexner", "truecase", "parse", "depparse", "dcoref", "relation", "natlog", "quote", "sentiment"]
 
 #Calling basic function which would return a 'requests' object
 out = cn.basic(data, out_format='json')
 ```
 Remember 'out' would be 'requests' object, you can get information by using out.text or out.json()
 
-Pywrap does not inherently support 'Sentiment' now as the downloadable server version of CoreNLP doesn't have 'Sentiment' support. But there is a hack for you to use (if you are sure that your server version is the newest one and has the support)
-- You can give sentiment as an attribute to annotator_list while instantiating the class object
-- or
-```
-annotator_list = CoreNLP.full_annotator_list + ['sentiment']
-```
-    
+
 ####Custom Function
 - arrange() can be used for getting formatted output
 - Format is given below
--- corenlp_pywrap/pywrap.py
+++ corenlp_pywrap/pywrap.py
@@ -12,19 +12,16 @@ root.addHandler(lhandler)
 
 class CoreNLP:
     root.debug('Object instantiating..')
-    annotator_full_list = ["tokenize", "cleanxml", "ssplit", "pos", 
-    "lemma", "ner", "regexner", "truecase", "parse", "depparse", "dcoref", 
-    "relation", "natlog", "quote"]
+    annotator_full_list = ["tokenize", "cleanxml", "ssplit", "pos",
+    "lemma", "ner", "regexner", "truecase", "parse", "depparse", "dcoref",
+    "relation", "natlog", "quote", "sentiment"]
     url = 'http://127.0.0.1:9000'
     out_format = 'json'
 
-    def __init__(self, url=url, annotator_list=annotator_full_list):        
+    def __init__(self, url=url, annotator_list=annotator_full_list):
         assert url.upper().startswith('HTTP'), \
             'url string should be prefixed with http'
-        if 'SENTIMENT' in map(str.upper, annotator_list):
-            root.warning('You are using "Sentiment" annotator which is'\
-                'not supported by Old version of CoreNLP')
-            
+
         if url.endswith('/'):
             self.url = url[:-1]
         else:
@@ -36,27 +33,22 @@ class CoreNLP:
             root.info('Using all the annotators, It might take a while')
 
         self.annotator_list = annotator_list
-        
-        common=set(self.annotator_list).intersection(self.annotator_full_list)
-        not_suprtd_elem = set(self.annotator_list) - common
-        assertion_error = 'annotator not supported: ' + str(not_suprtd_elem)
-        assert not not_suprtd_elem, assertion_error
 
 
     @staticmethod
     def server_connection(current_url, data):
         root.debug('server connection: ' + current_url)
         try:
-            server_out = requests.post(current_url, 
-                                        data, 
+            server_out = requests.post(current_url,
+                                        data,
                                         headers={'Connection': 'close'})
         except requests.exceptions.ConnectionError:
             root.error('Connection Error, check you have server running')
             raise Exception('Check your CoreNLP Server status \n'
                 'if not sure, Check the pywrap doc for Server instantiation')
         return server_out
-    
-   
+
+
     def url_calc(self, serializer=''):
         s_string = '/?properties={"annotators": "'
         anot_string = ','.join(self.annotator_list)
@@ -70,17 +62,17 @@ class CoreNLP:
         format_list = ['JSON', 'XML', 'TEXT', 'SERIALIZED']
         assert out_format.upper() in format_list, \
             'output format not supported, check stanford doc'
-        
+
         if out_format.upper() == 'SERIALIZED' and not serializer:
             root.info(
-                'Default Serializer is using - ' + 
+                'Default Serializer is using - ' +
                 'edu.stanford.nlp.pipeline.ProtobufAnnotationSerializer')
             serializer = ('edu.stanford.nlp.pipeline.'
                 'ProtobufAnnotationSerializer')
-                
+
         current_url = self.url_calc(serializer)
         assert isinstance(data, str) and data, 'Enter valid string input'
-        
+
         return self.server_connection(current_url, data)
 
     @staticmethod
@@ -95,7 +87,7 @@ class CoreNLP:
 
     @classmethod
     def regex(cls, endpoint, data, pattern, custom_filter):
-        url_string = '/?pattern=' + str(pattern) +'&filter=' + custom_filter 
+        url_string = '/?pattern=' + str(pattern) +'&filter=' + custom_filter
         current_url = cls.url + endpoint + url_string
         root.info('Returning the data requested')
         return cls.server_connection(current_url, data)
@@ -129,7 +121,7 @@ class CoreNLP:
                 #workaround to handle length inconsistancie with normalizedNER, rethink the logic
                 if 'ner' in val.keys() and 'normalizedNER' not in val.keys():
                     token_dict['normalizedNER'].append(0)
-                    
+
                 for key, val in val.items():
                     if key == 'index':
                         new_index = index + int(val)
@@ -148,7 +140,7 @@ class CoreNLP:
         assert isinstance(data, str) and data, 'Enter valid string input'
         if 'lemma' not in self.annotator_list:
             self.annotator_list.append('lemma')
-        
+
         current_url = self.url_calc()
         r = self.server_connection(current_url, data)
         try:
